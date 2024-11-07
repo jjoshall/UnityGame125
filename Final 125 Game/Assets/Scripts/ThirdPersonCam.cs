7a5bdf5d9aba876127f131efb754ced5ec95dd99
jowihall <148017202+jjoshall@users.noreply.github.com>
2024-11-07 13:58:27 -0800

    add player and camera

---
+++ Final 125 Game/Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used this video for Third-Person Movement: https://www.youtube.com/watch?v=UCwwn2q4Vys&ab_channel=Dave%2FGameDevelopment
+
+public class ThirdPersonCam : MonoBehaviour
+{
+     [Header("References")]
+     public Transform orientation;
+     public Transform player;
+     public Transform playerObj;
+     public Rigidbody rb;
+
+     public float rotationSpeed;
+
+     private void Start()
+     {
+          Cursor.lockState = CursorLockMode.Locked;
+          Cursor.visible = false;
+     }
+     
+     private void Update()
+     {
+          // rotate the orientation
+          Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+          orientation.forward = viewDir.normalized;
+
+          // rotate the player
+          float horizontalInput = Input.GetAxis("Horizontal");
+          float verticalInput = Input.GetAxis("Vertical");
+          Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+          if (inputDir != Vector3.zero)
+          {
+               playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+          }
+     }
+}
