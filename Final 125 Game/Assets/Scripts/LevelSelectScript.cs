6ef111be5c81020bc2d8ee3db954b02a354b7e0f
Dguerr15 <dguerr15@ucsc.edu>
2024-12-05 03:09:03 -0800

    level 3 and crystal edges fixed idk other stuff too

--- Final 125 Game/Assets/Scripts/LevelSelectScript.cs
+++ Final 125 Game/Assets/Scripts/LevelSelectScript.cs
@@ -7,7 +7,19 @@ using UnityEngine.UI;
 
 public class LevelSelectScript : MonoBehaviour
 {
-     public void GoToLevel1()
+    [Header("UI Elements")]
+    public Image[] Level1Stars;
+    public Image[] Level2Stars;
+    public Image[] Level3Stars;
+
+    [Header("Star Settings")]
+    public int totalCollectables = 2; // Total number of collectables per the level
+    public Sprite starSprite;
+    public Sprite emptyStarSprite;
+
+    
+
+    public void GoToLevel1()
      {
           // Load the first level
           SceneManager.LoadScene("Level1");
@@ -30,4 +42,33 @@ public class LevelSelectScript : MonoBehaviour
           // Load the main menu scene
           SceneManager.LoadScene("MainMenu");
      }
+
+    private void Start()
+    {
+        
+    }
+
+    // Called when the player wins the game
+    
+    private string GenerateStarDisplay(int collected)
+    {
+        [0].sprite = starSprite;
+
+        string result = "Level Completed\n";
+
+        for (int i = 0; i < totalCollectables; i++)
+        {
+            if (i < collected)
+            {
+                result += "Crystal Collected\n"; // Star earned
+                stars[i + 1].sprite = starSprite;
+            }
+            else
+            {
+                result += "Missing Crystal\n"; // Empty star
+            }
+        }
+
+        return result;
+    }
 }
